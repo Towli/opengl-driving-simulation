b069f85051f05cbbc19156e6174bd8d9b604eb65
Alex Towli <alextowli@hotmail.co.uk>
2016-12-11 17:01:39 +0000

    add sphere class

---
+++ Geometry/Sphere.h
@@ -0,0 +1,27 @@
+#include "..\gl\glew.h"
+
+class Shader;
+
+class Sphere
+{
+private:
+	unsigned int m_vaoID;		    // vertex array object
+	unsigned int m_vboID[3];		// two VBOs - used for colours, vertex and normals data
+	GLuint ibo;                     //identifier for the triangle indices
+
+	int numOfVerts;
+	int numOfTris;
+
+	float* verts;
+	float* norms;
+	unsigned int* tInds;
+
+	float cx,cy,cz; //centre of the sphere
+	float r;		//radius of the sphere
+public:
+	Sphere();
+	void constructGeometry(Shader* myShader, int level);
+	void render();
+	void setRadius(float rad);
+	void setCentre(float x, float y, float z);
+};
